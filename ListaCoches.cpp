b20389e045c20864ea8880f6be9d6f76cd0dcbe5
rankern <victorm.lafuente@gmail.com>
2018-03-01 20:16:49 +0000

    listaC 2

--- ListaCoches.cpp
+++ ListaCoches.cpp
@@ -1,3 +1,23 @@
-#include "ListaCoches.cpp"
+#include "ListaCoches.hpp"
 
 
+        //Crea una nueva lista
+        ListaCoches::ListaCoche(){
+
+        }
+        //Añade nuevo
+        void ListaCoches::AnyadirCoche(const Coche &c){
+            this->listCoche.push_back(c);
+        }
+        //Calcular ruta proxima
+        Coche ListaCoches::CalcularMasCercano(Ruta r){
+            for(Coche& c: this->listCoche){
+                if(!c.estaEnRuta() && (c.tiempoRestante()+   -r.obtobtenerMinComienzo)){
+
+                }
+            }
+        }
+        //
+        string ListaCoches::to_string(){
+
+        }
